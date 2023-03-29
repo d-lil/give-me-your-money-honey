ab92121dbb7a5f5f8c236de5e1318fa790efa314
Danny <dahneel@gmail.com>
2023-03-29 13:18:11 -0600

    Think I finished all of the code, it appears to be working in Insomnia

--- models/index.js
+++ models/index.js
@@ -13,20 +13,21 @@ Product.belongsTo(Category, {
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'product_id',
+  onDelete: "CASCADE",
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
     unique: false
-  },
+  }, as: "product_tags"
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
     unique: false
-  },
+  }, as: "tag_products",
 });
 module.exports = {
   Product,
